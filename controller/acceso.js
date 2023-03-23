712699f452886bf8da6161723d0207f0ad547753
Andho-Catalogo <127954660+Andho-Catalogo@users.noreply.github.com>
2023-03-22 22:28:43 -0300

    Config login
    Configuration for login post method

--- controller/acceso.js
+++ controller/acceso.js
@@ -2,18 +2,28 @@ const formulario = document.querySelector('#login');
 const inUsuario = document.querySelector('#usuario');
 const inPassword = document.querySelector('#password');
 
-const getConexion = () => fetch('https://andhocatalogo-production.up.railway.app/usuarios').then((res) => res.json());
+const getConexion = () => fetch('https://andhocatalogo-production.up.railway.app/usuarios', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        nombre: inUsuario.value,
+        contrasena: inPassword.value
+    })
+})
 
-getConexion().then((data) => {
-    formulario.addEventListener('submit', (e) => {
-        e.preventDefault();
-        if ((data[0].nombre) == inUsuario.value && (data[0].contraseña) == inPassword.value) {
-            window.location.href = './index.html?logged=true';
-        } else {
-            Swal.fire({
-                icon: 'error',
-                text: 'Verifica usuario o contraseña',
-            });
-        }
-    });
-});
+formulario.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    await getConexion()
+        .then((response) => {
+            if (response.ok) {
+                window.location.href = './index.html?logged=true';
+            }else{
+                Swal.fire({
+                    icon: 'error',
+                    text: 'Verifica usuario o contraseña',
+                });
+            }
+        });
+})
