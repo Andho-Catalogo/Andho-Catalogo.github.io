08def0737d9248e53deb2b50d0a003f1d2a8f5e1
ANDHO <regresoalsantuario@gmail.com>
2023-04-06 00:07:37 -0300

    Cambio en el codigo de filtros

--- controller/index-controllers/filtros.js
+++ controller/index-controllers/filtros.js
@@ -1,59 +1,13 @@
-export const filtrarZapatos = () => {
-    const zapatos = document.querySelector('#zapatos')
-    zapatos.addEventListener('click', (e) => {
-        const tipoZapato = document.querySelectorAll('#tipo')
-        tipoZapato.forEach(elemento => {
-            if (elemento.innerHTML !== "zapato") {
+export const filtrar = () => {
+    const items = document.querySelector('#items')
+    items.addEventListener('click', (e)=>{
+        const tipoProducto = document.querySelectorAll('#tipo')
+        tipoProducto.forEach(elemento => {
+             if(e.target.id !== "todos" && elemento.innerHTML !== e.target.innerHTML.toLowerCase()){
                 elemento.parentNode.parentNode.style.display = 'none'
-            }
-            if(elemento.innerHTML == "zapato"){
+             }else{
                 elemento.parentNode.parentNode.style.display = 'block'
-            }
-        })
-    })
-}
-
-export const filtrarCarteras = () => {
-    const carteras = document.querySelector('#carteras')
-    carteras.addEventListener('click', (e) => {
-        const tipoCartera = document.querySelectorAll('#tipo')
-        tipoCartera.forEach(elemento => {
-            if (elemento.innerHTML !== "cartera") {
-                elemento.parentNode.parentNode.style.display = 'none'
-            }
-            if(elemento.innerHTML == "cartera"){
-                elemento.parentNode.parentNode.style.display = 'block'
-            }
-        })
-    })
-}
-
-export const filtrarNeceser = () => {
-    const neceser = document.querySelector('#neceser')
-    neceser.addEventListener('click', (e) => {
-        const tipoNeceser = document.querySelectorAll('#tipo')
-        tipoNeceser.forEach(elemento => {
-            if (elemento.innerHTML !== "neceser") {
-                elemento.parentNode.parentNode.style.display = 'none'
-            }
-            if(elemento.innerHTML == "neceser"){
-                elemento.parentNode.parentNode.style.display = 'block'
-            }
-        })
-    })
-}
-
-export const filtrarMochila = () => {
-    const mochilas = document.querySelector('#mochila')
-    mochilas.addEventListener('click', (e) => {
-        const tipoMochia = document.querySelectorAll('#tipo')
-        tipoMochia.forEach(elemento => {
-            if (elemento.innerHTML !== "mochila") {
-                elemento.parentNode.parentNode.style.display = 'none'
-            }
-            if(elemento.innerHTML == "mochila"){
-                elemento.parentNode.parentNode.style.display = 'block'
-            }
+             }
         })
     })
 }
@@ -63,7 +17,7 @@ export const noFiltrar = () => {
     todos.addEventListener('click', (e)=>{
         const cards = document.querySelectorAll('.col')
         cards.forEach(elemento =>{
-            if(elemento.style.display = 'none'){
+            if(elemento.style.display == 'none'){
                 elemento.style.display = 'block'
             }
         })
